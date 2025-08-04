5cdc15e6a28803a70b7785ab109ad2a4a886b7e1
Manrique1601836 <manriqueedwin558@gmail.com>
2025-08-03 21:12:54 -0500

    parte 3

--- src/view/About.jsx
+++ src/view/About.jsx
@@ -1,8 +1,51 @@
+
+import '../style/view/About.css'
+import Woman from '../img/Woman.jpg'
+import img1 from '../img/img1.jpg'
+import img2 from '../img/img2.jpg'
+import img3 from '../img/img3.jpg'
+import img4 from '../img/img4.jpg'
+import img5 from '../img/img5.jpg'
+
 function About() {
     return(
         <>
             <main>
-                <h1>About</h1>
+                <div className="main-about">
+                    <div className="about-one">
+                        <div className="about-texto">
+                           <p>
+                            En D&D entendemos que la moda va más allá de seguir tendencias; 
+                            es una forma de contar quién eres sin decir una palabra. Nuestra
+                            tienda nace con la visión de crear un espacio donde cada persona
+                            pueda encontrar ropa con estilo, identidad y carácter. Nos especializamos
+                            en prendas cuidadosamente seleccionadas y diseñadas, combinando calidad,
+                            comodidad y un toque único que distingue a nuestra marca. En D&D no solo
+                            vendemos ropa, ofrecemos una experiencia: desde la atención personalizada
+                            hasta cada detalle en nuestros diseños. Creemos en el poder de vestir 
+                            con seguridad, en sentirse bien por dentro y por fuera, y en la libertad
+                            de expresar tu personalidad a través de lo que usas. Ya sea para un
+                            look casual, urbano o más elegante, en D&D encontrarás algo que se
+                            adapte a ti y a tu estilo de vida. Gracias por confiar en nosotros
+                            y ser parte de esta comunidad que valora la autenticidad y el buen
+                            gusto. Bienvenid@ a D&D, donde tu estilo encuentra su mejor versión.
+                           </p>
+                    </div>
+                    <div className="about-imagen">
+                        <img src={Woman} alt="" />
+                    </div>
+                    </div>
+
+                    <div className="about-two">
+                        <div className="two-img">
+                            <img src={img1} alt="" className='img1'/>
+                            <img src={img2} alt="" className='img2'/>
+                            <img src={img3} alt="" className='img3'/>
+                            <img src={img4} alt="" className='img4'/>
+                            <img src={img5} alt="" className='img5'/>
+                        </div>
+                    </div>
+                </div>
             </main>
         </>
     )
--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,64 +1,15 @@
 import { useEffect, useState } from "react"
+import '../style/view/Home.css'
 
 function Home() {
 
-    const [dato, setDato] = useState(() => {
-        const datoGuardado = localStorage.getItem("contador")
-        return datoGuardado !== null ? Number(datoGuardado) : 0
-    })
-
-
-    const [letras, setLetra] = useState(() => {
-        const textoGuardada = localStorage.getItem("letraGuardada")
-        return textoGuardada !== null ? textoGuardada : ""
-    })
-
-
-    const [inputtexto,setInputtexto] = useState("")
-
-    useEffect(() => {
-        console.log("Guardando dato:", dato)
-        localStorage.setItem("contador", dato)
-    }, [dato])
-
-    useEffect(() => {
-        console.log("Guardando dato:", letras)
-        localStorage.setItem("letraGuardada", letras)
-    }, [letras])
-
-    const aumentar = () => {
-        setDato(dato + 1)
-    }
-
-    const disminuir = () => {
-        setDato(dato - 1)
-    }
-
-    const guardartexto = (e) => {
-        e.preventDefault()
-        setLetra(inputtexto)
-        setInputtexto("")
-    }
+    
 
     return (
         <>
             <main>
-                <div className="box">
-                    <p>{dato}</p>
-                    <button onClick={aumentar}>Aumentar</button>
-                    <button onClick={disminuir}>Disminuir</button>
-                </div>
-                <div className="box">
-                    <p>{letras}</p>
-                    <form onSubmit={guardartexto}>
-                        <input 
-                            type="text" 
-                            value={inputtexto}
-                            onChange={(e) => setInputtexto(e.target.value)}
-                            placeholder="Escribe algo"
-                        />
-                        <button type="submit">agregar</button>
-                    </form>
+                <div className="main-home">
+                    <p>Elige ser tu propio estilo</p>
                 </div>
             </main>
         </>
